38bf6075fcc77a02d3fea2f56fceb9737fb6de9e
Bogdan Pankevych <>
2024-10-16 19:24:07 +0200

    initial commit

---
+++ BPRestaurant/BPRestaurant/BPRestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  BPRestaurantApp.swift
+//  BPRestaurant
+//
+//  Created by admin on 16.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct BPRestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
